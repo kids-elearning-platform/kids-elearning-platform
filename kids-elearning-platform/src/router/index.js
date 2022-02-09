317c191dccd2034dfddcae359077a8c4e80b7aa3
Hamidkacem <hamidkacem4@gmail.com>
2022-02-10 00:30:34 +0100

    someChangeView

--- kids-elearning-platform/src/router/index.js
+++ kids-elearning-platform/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
+
 const routes = [
   {
     path: "/",
@@ -8,7 +9,7 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
+    path: "/About",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
@@ -16,6 +17,24 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/challenge",
+    name: "Challenge",
+    component: () =>
+    import(/* webpackChunkName: "Challenge" */ "../views/Challenge.vue"),
+  },
+  {
+    path: "/feedback",
+    name: "Feedback" ,
+    component: () =>
+    import(/* webpackChunkName: "Feedback" */ "../views/Feedback.vue"),
+  },
+  {
+    path: "/contact",
+    name: "Contact" ,
+    component: () =>
+    import(/* webpackChunkName: "Contact" */ "../views/Contact.vue"),
+  }
 ];
 
 const router = createRouter({
---
+++ kids-elearning-platform/src/views/Animals.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Animals</div>
+</template>
+
+<script>
+export default {
+  name: "Animals",
+}
+</script>
+
+<style>
+
+
+</style>
