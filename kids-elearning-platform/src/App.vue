3cd810854100f787c5ac9d9cd437ff8b47a7223d
unknown <Ryanekh@gmail.com>
2022-02-10 17:03:09 +0100

    challenge design and nav bar design => start on functions and rendring data

--- kids-elearning-platform/src/App.vue
+++ kids-elearning-platform/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav>
+  <nav id="navv">
     <i class="fas fa-bars"></i>
 
     <label class="logo">Kids World</label>
-    <ul>
+    <ul id="stt">
       <li><router-link to="/">Home</router-link></li>
       <li><router-link to="/About">About</router-link></li>
       <li><router-link to="/Contact">Contact</router-link></li>
@@ -84,6 +84,20 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
+  
+}
+#navv{
+    background-color: rgba(255, 228, 196, 0.603);
+  margin-left: 0px;
+  margin-top: 4px;
+  border-radius: 5px;
+  background: rgba(189, 17, 17, 0.61);
+  box-shadow: 0 8px 32px 0 rgba(14, 2, 37, 0.705);
+  backdrop-filter: blur( 4.5px );
+  -webkit-backdrop-filter: blur( 4.5px );
+  border-radius: 10px;
+  border: 1px solid rgba(15, 4, 44, 0.18);
 }
 
 #nav {
@@ -97,5 +111,8 @@ body {
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+#stt{
+  
 }
 </style>
