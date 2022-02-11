af0ce894e3d1efde83b3ea7eb7234c43142730ae
Hamidkacem <hamidkacem4@gmail.com>
2022-02-11 23:44:43 +0100

    Color / Animal / Number/Fruit Commponet

--- kids-elearning-platform/src/views/Color.vue
+++ kids-elearning-platform/src/views/Color.vue
@@ -1,60 +1,91 @@
 <template>
   <div>
     <div id="box">
-      <div  @click="playSound(sounds[index])" class="titleContainer title">
-        <img id="question" src="https://images-ext-2.discordapp.net/external/PoZU3jOHMn73f-gr_mB5-zefsxjS5DTS7JyKqRw0zQI/https/cdn3d.iconscout.com/3d/premium/thumb/microphone-3711800-3105274.png?width=433&height=433" />
+      <div @click="playSound(sounds[index])" class="titleContainer title">
+        <img
+          id="question"
+          src="https://images-ext-2.discordapp.net/external/PoZU3jOHMn73f-gr_mB5-zefsxjS5DTS7JyKqRw0zQI/https/cdn3d.iconscout.com/3d/premium/thumb/microphone-3711800-3105274.png?width=433&height=433"
+        />
       </div>
-<div>
-    <div class="answer" v-for="(question,index) in questions[index].suggestions" v-bind="key" :key="index" @click="next(index)">
-     
-     <img :src="question.suggestion" alt=""/> 
-    </div>
-  </div> 
+      <div>
+        <div
+          class="answer"
+          v-for="(question, index) in questions[index].suggestions"
+          v-bind="key"
+          :key="index"
+          @click="next(index)"
+        >
+          <img id="img" :src="question.suggestion" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import lion from "../../public/sounds/Lion.mp3";
-import duck from "../../public/sounds/Duck.mp3";
-import elephant from "../../public/sounds/elephant.mp3";
-import butterfly from "../../public/sounds/butterfly.mp3";
-import owl from "../../public/sounds/Owl.mp3";
+import red from "../../public/sounds/red.mp3";
+import oranges from "../../public/sounds/oranges.mp3";
+import green from "../../public/sounds/green.mp3";
+
 export default {
   name: "Color",
   data() {
-    
     return {
-      sounds: [lion, duck, butterfly, owl, elephant],
+      sounds: [red, oranges, green],
       score: 0,
       index: 0,
       questions: [
         {
           suggestions: [
-            { suggestion: 'https://images-ext-2.discordapp.net/external/Lwv9yAZJObAzdxaXB1_fDeZ2ZK-gvSwqTvilK5IViV8/https/cdn3d.iconscout.com/3d/premium/thumb/color-bucket-3981966-3297109%400.png?width=433&height=433' },
-            { suggestion: "🦁", correct: true },
-            { suggestion: "🐜" },
-            { suggestion: "🐞" },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941816440175071262/orange-8_4507176.jpg?width=768&height=432",
+            },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941816440422539264/download.png",
+              correct: true,
+            },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941817049712320523/download_1.png",
+            },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941817255531012156/640px-A_black_image.jpg?width=576&height=432",
+            },
           ],
         },
         {
           suggestions: [
-            { suggestion: "🐭" },
-            { suggestion: "🦆", correct: true },
-            { suggestion: "🐴" },
-            { suggestion: "🐳" },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941817613456130048/2560px-Purple.svg.png?width=648&height=432",
+            },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941816440175071262/orange-8_4507176.jpg?width=768&height=432",
+              correct: true,
+            },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941817613854580786/Solid_pink.svg.png?width=432&height=432",
+            },
+            {
+              suggestion:
+                "https://media.discordapp.net/attachments/941079224666697738/941817996333154314/768px-Solid_blue.svg.png?width=432&height=432",
+            },
           ],
         },
         {
           suggestions: [
-            { suggestion: "🦋", correct: true },
-            { suggestion: "🐒" },
-            { suggestion: "🐪" },
-            { suggestion: "🐷" },
+            { suggestion: "https://media.discordapp.net/attachments/941079224666697738/941816440644853891/F1_green_flag.svg.png?width=576&height=432", correct: true },
+            { suggestion: "https://media.discordapp.net/attachments/941079224666697738/941817996333154314/768px-Solid_blue.svg.png?width=432&height=432" },
+            { suggestion: "https://media.discordapp.net/attachments/941079224666697738/941817613456130048/2560px-Purple.svg.png?width=648&height=432" },
+            { suggestion: "https://media.discordapp.net/attachments/941079224666697738/941817049712320523/download_1.png" },
           ],
         },
         //🦍🐘🐊🦈🐓🦉🐧
       ],
-    
     };
   },
   methods: {
@@ -72,50 +103,49 @@ export default {
       alert("navigation was prevented."); // to test if functions are passed
     },
     next() {
-    
       if (this.index < 2) {
-        this.index++
-        
-        }
- 
-  }, scores: function() {
-         var score = 0;
-         for (let i = 0; i < this.userResponses.length; i++) {
-            if (
-               typeof this.questions[i].responses[
-                  this.userResponses[i]
-               ] !== "undefined" &&
-               this.quiz.questions[i].responses[this.userResponses[i]].correct
-            ) {
-               score = score + 1;
-            }
-         }
-         return score;
-
-         //return this.userResponses.filter(function(val) { return val }).length;
+        this.index++;
       }
-   }
-}
+    },
+    scores: function () {
+      var score = 0;
+      for (let i = 0; i < this.userResponses.length; i++) {
+        if (
+          typeof this.questions[i].responses[this.userResponses[i]] !==
+            "undefined" &&
+          this.quiz.questions[i].responses[this.userResponses[i]].correct
+        ) {
+          score = score + 1;
+        }
+      }
+      return score;
+
+      //return this.userResponses.filter(function(val) { return val }).length;
+    },
+  },
+};
 </script>
 <style scoped>
-#question{
+#img {
+  border-radius: 50%;
+  width: 250px;
+  height: 250px;
+}
+#question {
   height: 150px;
   width: 150px;
-font-size: 3cm;
-cursor: pointer;
+  font-size: 3cm;
+  cursor: pointer;
 }
-.answer{
+.answer {
   display: inline;
-;
-font-size: 5cm;
-padding-left: 0cm;
-padding-right: 5cm;
-margin-left: 4cm;
-cursor: pointer;
-
-
+  font-size: 5cm;
+  padding-left: 0cm;
+  padding-right: 5cm;
+  margin-left: 4cm;
+  cursor: pointer;
 }
-#box{
+#box {
   height: 700px;
   width: 1500px;
   background-color: rgba(255, 228, 196, 0.603);
@@ -124,20 +154,9 @@ cursor: pointer;
   border-radius: 5px;
   background: rgba(5, 5, 5, 0.61);
   box-shadow: 0 8px 32px 0 rgba(14, 2, 37, 0.705);
-  backdrop-filter: blur( 4.5px );
-  -webkit-backdrop-filter: blur( 4.5px );
+  backdrop-filter: blur(4.5px);
+  -webkit-backdrop-filter: blur(4.5px);
   border-radius: 10px;
   border: 1px solid rgba(15, 4, 44, 0.18);
 }
 </style>
-
-
-
-
-
-
-
-
-
-
-
--- kids-elearning-platform/src/views/Number.vue
+++ kids-elearning-platform/src/views/Number.vue
@@ -1,14 +1,174 @@
 <template>
-  <h1>Number</h1>
+  <div>
+    <div id="box">
+      <div
+        id="question"
+        @click="playSound(sounds[index])"
+        class="titleContainer title"
+      >
+        🔊
+      </div>
+      <div>
+        <div
+          class="answer"
+          v-for="(question, index) in questions[index].suggestions"
+          v-bind="key"
+          :key="index"
+          @click="next(index)"
+        >
+          <img id="img" :src="question.suggestion" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
+import number3 from "../../public/sounds/number3.mp3";
+import number2 from "../../public/sounds/number2.mp3";
+import number6 from "../../public/sounds/number6.mp3";
+import number5 from "../../public/sounds/number5.mp3";
+
 export default {
   name: "Number",
-}
+  data() {
+    return {
+      sounds: [number3, number2, number6, number5],
+      score: 0,
+      index: 0,
+      questions: [
+        {
+          suggestions: [
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/bqlYwU1oGL3swILLp2qBksk2Bj7Te2UmswRzMh_4Iog/https/cdn3d.iconscout.com/3d/premium/thumb/three-number-4863643-4056289.png?width=433&height=433",
+            },
+            {
+              suggestion:
+                "https://images-ext-1.discordapp.net/external/GqMHGoFPwPHrmi28WDpz54lrzjSe6Scsi8Toj-G3nz4/https/cdn3d.iconscout.com/3d/premium/thumb/seven-number-4863644-4056290.png?width=433&height=433",
+              correct: true,
+            },
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/UOfr3p17NbEZh6u3KdxEh4MQtlstOcdHP8lO30cfPOE/https/cdn3d.iconscout.com/3d/premium/thumb/five-number-4863647-4056293.png?width=433&height=433",
+            },
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/eX-qTZT6N-secy7zNxRddwidFPnRhvCTlnBcBUlYCH0/https/cdn3d.iconscout.com/3d/premium/thumb/comma-sign-4863650-4056296.png?width=433&height=433",
+            },
+          ],
+        },
+        {
+          suggestions: [
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/ARbyf1ozzv81gNLPyZpjCif2h9CEQKW5IFItT4jxywI/https/cdn3d.iconscout.com/3d/premium/thumb/two-number-4863651-4056297.png?width=433&height=433",
+            },
+            {
+              suggestion:
+                "https://images-ext-1.discordapp.net/external/GqMHGoFPwPHrmi28WDpz54lrzjSe6Scsi8Toj-G3nz4/https/cdn3d.iconscout.com/3d/premium/thumb/seven-number-4863644-4056290.png?width=433&height=433",
+              correct: true,
+            },
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/bqlYwU1oGL3swILLp2qBksk2Bj7Te2UmswRzMh_4Iog/https/cdn3d.iconscout.com/3d/premium/thumb/three-number-4863643-4056289.png?width=433&height=433",
+            },
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/clEANbX-G9V5Cuf4gzflcsUap43l7evK6Sae0t-xt50/https/cdn3d.iconscout.com/3d/premium/thumb/six-number-4863652-4056298.png?width=433&height=433",
+            },
+          ],
+        },
+        {
+          suggestions: [
+            {
+              suggestion:
+                "https://images-ext-1.discordapp.net/external/nAAd4aTBeDrdQk0ABw7HexXky5zyjmqDBl9Zl4TAZPA/https/cdn3d.iconscout.com/3d/premium/thumb/four-number-4863648-4056294.png?width=433&height=433",
+              correct: true,
+            },
+            {
+              suggestion:
+                "https://images-ext-1.discordapp.net/external/wIUkqZFwM5NPkjMsXiUnhIxMB6XYymLDIEtrzsOnSMM/https/cdn3d.iconscout.com/3d/premium/thumb/eight-number-4863641-4056287.png?width=433&height=433",
+            },
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/clEANbX-G9V5Cuf4gzflcsUap43l7evK6Sae0t-xt50/https/cdn3d.iconscout.com/3d/premium/thumb/six-number-4863652-4056298.png?width=433&height=433",
+            },
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/UOfr3p17NbEZh6u3KdxEh4MQtlstOcdHP8lO30cfPOE/https/cdn3d.iconscout.com/3d/premium/thumb/five-number-4863647-4056293.png?width=433&height=433",
+            },
+          ],
+        },
+        //🦍🐘🐊🦈🐓🦉🐧
+      ],
+    };
+  },
+  methods: {
+    playSound(sound) {
+      if (sound) {
+        var audio = new Audio(sound);
+        audio.play();
+      }
+    },
+    sound() {
+      var audio = new Audio(require("../../public/sounds/Lion.mp3"));
+      audio.play();
+    },
+    notify() {
+      alert("navigation was prevented."); // to test if functions are passed
+    },
+    next() {
+      if (this.index < 2) {
+        this.index++;
+      }
+    },
+    scores: function () {
+      var score = 0;
+      for (let i = 0; i < this.userResponses.length; i++) {
+        if (
+          typeof this.questions[i].responses[this.userResponses[i]] !==
+            "undefined" &&
+          this.quiz.questions[i].responses[this.userResponses[i]].correct
+        ) {
+          score = score + 1;
+        }
+      }
+      return score;
+
+      //return this.userResponses.filter(function(val) { return val }).length;
+    },
+  },
+};
 </script>
-
-<style>
-
-
+<style scoped>
+#img {
+  width: 250px;
+  height: 250px;
+}
+#question {
+  font-size: 3cm;
+  cursor: pointer;
+}
+.answer {
+  display: inline;
+  font-size: 5cm;
+  padding-left: 0cm;
+  padding-right: 5cm;
+  margin-left: 4cm;
+  cursor: pointer;
+}
+#box {
+  height: 700px;
+  width: 1500px;
+  background-color: rgba(255, 228, 196, 0.603);
+  margin-left: 200px;
+  margin-top: 40px;
+  border-radius: 5px;
+  background: rgba(5, 5, 5, 0.61);
+  box-shadow: 0 8px 32px 0 rgba(14, 2, 37, 0.705);
+  backdrop-filter: blur(4.5px);
+  -webkit-backdrop-filter: blur(4.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(15, 4, 44, 0.18);
+}
 </style>
