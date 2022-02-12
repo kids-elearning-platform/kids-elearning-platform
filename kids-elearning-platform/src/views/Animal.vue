97421ea12b2071aa1cea27aaac73059bb87c8be5
Hamidkacem <hamidkacem4@gmail.com>
2022-02-12 04:37:12 +0100

    Score Comp

--- kids-elearning-platform/src/views/Animal.vue
+++ kids-elearning-platform/src/views/Animal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="changeview === false">
     <div id="box">
       <div
         id="question"
@@ -14,13 +14,39 @@
           v-for="(question, index) in questions[index].suggestions"
           v-bind="key"
           :key="index"
-          @click="next(index)"
+          @click="next"
         >
-          <img id="img" :src="question.suggestion" />
+          <img
+            id="img"
+            :src="question.suggestion"
+            @click="userresponse(question.correct)"
+          />
         </div>
       </div>
     </div>
   </div>
+  <div v-if="changeview === true">
+    <div>
+      <div id="box">
+        {{this.test()}}
+       
+        <div class="parent">
+         
+       
+          <div>
+          <img id="img2" :src="this.photo"/>
+          </div>
+       
+          <div>
+          <h1 id='p'></h1>
+          </div>
+       
+          <div>
+          <img id="img1" src="https://images-ext-2.discordapp.net/external/bqlYwU1oGL3swILLp2qBksk2Bj7Te2UmswRzMh_4Iog/https/cdn3d.iconscout.com/3d/premium/thumb/three-number-4863643-4056289.png?width=433&height=433"/></div></div>
+          </div>
+      </div>
+  </div> 
+  
 </template>
 <script>
 import lion from "../../public/sounds/Lion.mp3";
@@ -32,9 +58,13 @@ export default {
   name: "Animal",
   data() {
     return {
+     photo:"https://images-ext-2.discordapp.net/external/6gjcxtvNyvEzUE_q3GkZL6JJG2uvcID3O5HYR0HNeTI/https/cdn3d.iconscout.com/3d/premium/thumb/1-number-4863653-4056299.png?width=432&height=432",
+      response: [],
+      changeview: false,
       sounds: [lion, duck, butterfly, owl, elephant],
       score: 0,
       index: 0,
+
       questions: [
         {
           suggestions: [
@@ -80,15 +110,23 @@ export default {
         },
         {
           suggestions: [
-            { suggestion:"https://images-ext-2.discordapp.net/external/1FTvVvSqhQ02LGjku9nXfSTy1GgEOT8OoMnCcBf48bM/https/cdn3d.iconscout.com/3d/premium/thumb/butterfly-4609618-3811472.png?width=433&height=433", correct: true },
             {
-              suggestion:"https://images-ext-2.discordapp.net/external/WFNNdtg65qqYVjUwZUuk0hWFklsnaXCApnqPkmv2mTQ/https/cdn3d.iconscout.com/3d/premium/thumb/rabbit-3690191-3081431.png?width=433&height=433",
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/1FTvVvSqhQ02LGjku9nXfSTy1GgEOT8OoMnCcBf48bM/https/cdn3d.iconscout.com/3d/premium/thumb/butterfly-4609618-3811472.png?width=433&height=433",
+              correct: true,
+            },
+            {
+              suggestion:
+                "https://images-ext-2.discordapp.net/external/WFNNdtg65qqYVjUwZUuk0hWFklsnaXCApnqPkmv2mTQ/https/cdn3d.iconscout.com/3d/premium/thumb/rabbit-3690191-3081431.png?width=433&height=433",
             },
             {
               suggestion:
                 "https://images-ext-1.discordapp.net/external/RvjzU7I2-zeCuI8stzAh12XpIjefwuyX7qeworY7krw/https/cdn3d.iconscout.com/3d/premium/thumb/fox-4609630-3811484.png?width=433&height=433",
             },
-            { suggestion: "https://images-ext-1.discordapp.net/external/wGnUvMvoHh18TGnVtrJ5pIFOt2XHeWTSTZluBicm3fo/https/cdn3d.iconscout.com/3d/premium/thumb/bird-4769077-3972350.png?width=433&height=433" },
+            {
+              suggestion:
+                "https://images-ext-1.discordapp.net/external/wGnUvMvoHh18TGnVtrJ5pIFOt2XHeWTSTZluBicm3fo/https/cdn3d.iconscout.com/3d/premium/thumb/bird-4769077-3972350.png?width=433&height=433",
+            },
           ],
         },
         //🦍🐘🐊🦈🐓🦉🐧
@@ -96,6 +134,29 @@ export default {
     };
   },
   methods: {
+    test(){
+      if(this.score===0){
+        this.photo="https://images-ext-2.discordapp.net/external/KKhnER3qr6BJASM64Q5R1mWDuDA3dhL2Keo8cCiaxJY/https/cdn3d.iconscout.com/3d/premium/thumb/zero-number-4863645-4056291.png?width=432&height=432"
+      }
+     if(this.score===1){
+
+       console.log('aaaaaaaaaaaaaaa');
+       this.photo="https://images-ext-2.discordapp.net/external/6gjcxtvNyvEzUE_q3GkZL6JJG2uvcID3O5HYR0HNeTI/https/cdn3d.iconscout.com/3d/premium/thumb/1-number-4863653-4056299.png?width=432&height=432"
+     }if(this.score===2){
+
+       console.log('aaaaaaaaaaaaaaa');
+       this.photo="https://images-ext-2.discordapp.net/external/ARbyf1ozzv81gNLPyZpjCif2h9CEQKW5IFItT4jxywI/https/cdn3d.iconscout.com/3d/premium/thumb/two-number-4863651-4056297.png?width=433&height=433"
+     }
+    },
+    userresponse(e) {
+      console.log(this.index, "length");
+      console.log(this.response, "response");
+      if (e) {
+        this.score = this.score + 1;
+        console.log(this.score, "hani lena");
+      }
+    },
+
     playSound(sound) {
       if (sound) {
         var audio = new Audio(sound);
@@ -110,12 +171,14 @@ export default {
       alert("navigation was prevented."); // to test if functions are passed
     },
     next() {
-      if (this.index < 2) {
-        this.index++;
+      this.index += 1;
+      if (this.index === 3) {
+        this.changeview = !this.changeview;
       }
     },
     scores: function () {
       var score = 0;
+
       for (let i = 0; i < this.userResponses.length; i++) {
         if (
           typeof this.questions[i].responses[this.userResponses[i]] !==
@@ -133,6 +196,20 @@ export default {
 };
 </script>
 <style scoped>
+#img1{
+  margin-left:-600px;
+  margin-top: 90px;
+  width: 250px;
+  height: 250px;
+}
+#img2{
+  width: 200px;
+  height: 200px;
+  margin-right: -600px ;
+  margin-top: -130px;
+  
+}
+
 #img {
   width: 250px;
   height: 250px;
@@ -162,5 +239,25 @@ export default {
   -webkit-backdrop-filter: blur(4.5px);
   border-radius: 10px;
   border: 1px solid rgba(15, 4, 44, 0.18);
+}
+.score {
+  
+  color: white;
+  font-size: 40px;
+}
+#p{
+  width:10px; 
+  height:200px;
+  background-color: white;
+  transform: rotate(18deg);
+  margin-left: 240px;
+}
+.parent {
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: 3fr;
+grid-column-gap: 0px;
+grid-row-gap: 0px;
+margin-top: 300px;
 }
 </style>
